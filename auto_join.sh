962bde5f15b6bac6033d010b75d4070cd5375bee
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2021-06-07 20:59:17 +0200

    Improve scripts

--- auto_join.sh
+++ auto_join.sh
@@ -4,5 +4,5 @@ for folder in *.d
 do
     file=$(echo ${folder} | cut -d '.' -f 1)  
     echo "Joining ${file}"
-    cat "${folder}/${file}_p*" > $file
+    cat "${folder}/${file}_p"* > $file
 done
--- splitter.sh
+++ splitter.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
+set -e
 
-echo -n "" > .gitignore
+if [[ -n "$1" ]]
+then
+	list=$1
+else
+	list="table_*"
+	echo -n "" > .gitignore
+fi
 
-for file in table_*
+
+for file in ${list}
 do
-	if [ -f $file ]
+	if [ -f "$file" ]
 	then
-		size=$(wc -c "$file" | cut -f 1 -d ' ')
-		if [ $size -gt 50000000 ]
+		size=$(wc -c < "$file")
+		if [ "$size" -gt 50000000 ]
 		then
 			echo "Splitting $file"
-			split -C 50M $file "$file""_p" -d
-			mkdir -p "$file"".d"
-			for part in "$file"_p*
-			do
-				mv "$part" "$file"".d"
-			done
-			echo $file >> .gitignore
+			split -C 50M "$file" "${file}_p" -d
+			
+			mkdir -p "${file}.d"
+			mv "$file"_p* "${file}.d"
+			
+			[[ -z $1 ]] && echo "$file" >> .gitignore
 		fi
 	fi
 done
